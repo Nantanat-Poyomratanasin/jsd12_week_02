512be2d5212ac7f4111a93a7ff7d08b03c34325a
Nantanat-Poyomratanasin <nantanat.poyomratanasin@gmail.com>
2026-03-10 17:17:25 +0700

    add file JavaScript

---
+++ 06_bring-it-to-javascript.js
@@ -0,0 +1,20 @@
+let product_lists = ["product_id_1", "product_id_2"];
+
+const product_Detail1 = {
+     ProductId: "product_id_1",
+     productName: "Automatic Door and Detecting location collar",
+     price: 25000,
+     Features: ["High resistance and Waterproof","Wifi","long performance"]
+};
+
+
+const product_Detail2 = {
+     ProductId: "product_id_2",
+     productName: "Detecting location collar",
+     price: 15000,
+     Features: ["For indoor pet","Wifi","long performance"]
+};
+
+
+console.log(product_Detail1);
+console.log(product_Detail2);
